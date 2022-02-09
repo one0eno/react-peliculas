047621021d14a1db960d9e5273ba397f7930d2fd
jorge <jorge.arranz@gmail.com>
2022-02-09 11:47:06 +0100

    Creacion de Login y crear

---
+++ PeliculasApi/PeliculasApi/Controllers/CuentaController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using PeliculasApi.DTO;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliculasApi.Controllers
+{
+    [Route("api/cuentas")]
+    [ApiController]
+    public class CuentaController:ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+        private readonly SignInManager<IdentityUser> signInManager;
+        public CuentaController(
+            UserManager<IdentityUser> userManager, 
+            IConfiguration configuration,
+            SignInManager<IdentityUser> signInManager )
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost("crear")]
+        public async Task<ActionResult<RespuestaAtenticacion>> Crear([FromBody] CredencialesUsuario credenciales)
+        {
+            var usuario = new IdentityUser { UserName = credenciales.Email, Email = credenciales.Email };
+            var resultado = await userManager.CreateAsync(usuario,credenciales.Password);
+
+            //SI ES CORRECTO RETORNAMOS TOKEN
+            if (resultado.Succeeded)
+            {
+                return await ConstruirToken(credenciales);
+            }
+            else {
+                return BadRequest(resultado.Errors);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<RespuestaAtenticacion>> Login([FromBody] CredencialesUsuario credenciales)
+        {
+            var resultado = await signInManager.PasswordSignInAsync(
+                credenciales.Email, 
+                credenciales.Password,
+                isPersistent:false,
+                lockoutOnFailure:false);
+
+            if (resultado.Succeeded)
+            {
+                return await ConstruirToken(credenciales);
+            }
+            else {
+                return BadRequest("Login o password incorrectos");
+            }
+        }
+
+            private async Task<RespuestaAtenticacion> ConstruirToken(CredencialesUsuario credenciales)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("email", credenciales.Email),
+                
+
+            };
+
+            var usuario = await userManager.FindByEmailAsync(credenciales.Email);
+            var claimsDB = await userManager.GetClaimsAsync(usuario);
+
+            claims.AddRange(claimsDB);
+
+            //CONSTRUIMOS EL JWTs
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["llavejwt"]));
+            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+
+            //TIEMPO DE ASPIRACION DEL TOKEN
+            var expiracion = DateTime.UtcNow.AddYears(1);
+
+            //TOKEN
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims,
+                expires: expiracion, signingCredentials: creds);
+
+            return new RespuestaAtenticacion()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiracion = expiracion,
+
+            };
+
+        }
+
+
+    }
+}
---
+++ PeliculasApi/PeliculasApi/DTO/RespuestaAtenticacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasApi.DTO
+{
+    public class RespuestaAtenticacion
+    {
+       public string Token { get; set; }
+       public DateTime Expiracion { get; set; }
+    }
+}
