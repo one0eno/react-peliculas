27b8aef269a1056a71b94241bb491a9a6b8d3117
jorge <jorge.arranz@gmail.com>
2022-02-09 11:55:31 +0100

    añadimos en el parseador de bad request los errores que puede traer identity

--- PeliculasApi/PeliculasApi/Filtros/ParsearBadRequest.cs
+++ PeliculasApi/PeliculasApi/Filtros/ParsearBadRequest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using System;
@@ -31,13 +32,20 @@ namespace PeliculasApi.Filtros
 
 
                 }
-                else 
+                else if ( resultadoActual.Value is IEnumerable<IdentityError> errores) {
+
+                    foreach (var error in errores)
+                    {
+                        respuesta.Add(error.Description);
+                    }
+                }
+                else
                 {
                     foreach (var llave in context.ModelState.Keys)
                     {
                         foreach (var error in context.ModelState[llave].Errors)
                         {
-                          
+
                             respuesta.Add($"{llave}: {error.ErrorMessage}");
                         }
                     }
